6abf3943a1f10de074afb727f23885743ed420b7
DavidOla <davidolagunju96@gmail.com>
2016-02-15 15:32:07 +0000

    Create SoundButton.java

---
+++ SoundButton.java
@@ -0,0 +1,63 @@
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.util.*;
+import javax.swing.*;
+
+public class SoundButton extends JToggleButton {
+
+	private static final long serialVersionUID = -3562101876274080396L;
+	//The X and Y co-ord of the button
+	private final int x;
+	private final int y;
+	
+	private Tenori tenori;
+	
+	private static ArrayList<SoundButton> buttonsSelected = new ArrayList<SoundButton>();
+	//could also use arraylist like
+	
+	public SoundButton(int x, int y, Tenori tenori){
+		this.x = x;
+		this.y = y;
+		this.tenori = tenori;
+		
+		
+		//this.setContentAreaFilled(false);
+		this.setBorderPainted(false);
+		this.setFocusPainted(false);
+		
+		addMouseListener (new MouseAdapter(){
+			public void mouseClicked(MouseEvent me){
+				//produce sound
+			}
+		});		
+	}
+	
+	public static ArrayList<SoundButton> getButtonsSelected(){
+		return buttonsSelected;
+	}
+	
+	public static void addButtonsSelected(SoundButton e){
+		buttonsSelected.add(e);
+	}
+	
+	public static void removeButtonsSelected(SoundButton e){
+		buttonsSelected.remove(e);
+	}
+	
+	public static void clearButtonsSelected(SoundButton e){
+		buttonsSelected.clear();
+	}
+	
+	public int getX(){
+		return this.x;
+	}
+	
+	public int getY(){
+		return this.y;
+	}
+	
+	public Tenori getTenori(){
+		return this.tenori;
+	}
+}
