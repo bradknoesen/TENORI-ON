8a31c15fd4e6186d8a57121c52f1aa1bae7393e6
DavidOla <davidolagunju96@gmail.com>
2016-02-15 15:25:25 +0000

    Create TestSuite.java

---
+++ TestSuite.java
@@ -0,0 +1,13 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/*
+ * @author David Olagunju, Presley Kode
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TenoriTest.class})
+public class TestSuite{
+	
+}
