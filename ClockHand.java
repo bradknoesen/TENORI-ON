570908f33cfd53fac30775f9d07015d22504f464
sup3rmuff1n <bradknoesen@gmail.com>
2016-02-25 09:22:33 +0000

    Update ClockHand.java

--- ClockHand.java
+++ ClockHand.java
@@ -1,33 +1,51 @@
-import java.util.concurrent.atomic.*;
+import java.util.concurrent.atomic.AtomicBoolean;
 
-public class ClockHand {
-	private Tenori Tenori;
-	private int loop = 16;
-	public AtomicBoolean running = new AtomicBoolean();
-	
-	public ClockHand (Tenori Tenori){
-		this.Tenori = Tenori;
-	}
-	
-	public void run(){
-		System.out.println("Clockhand thread is running");
-		running.set(true);
-		
-		while(running.get()){
-			//clock hand pattern loop
-			for (int i=0; i < getLoop(); i++){
-				Tenori.clockHandHighlight(i);
-			}
-			
-			
-		}
-	}
-	
-	public void setLoop(int loop){
-		this.loop = loop;
-	}
-	
-	public int getLoop(){
-		return this.loop;
-	}
+public class ClockHand implements Runnable {
+
+    private Tenori te;
+    private int loopPoint = 16; //change this vairable for set Loop poiny 
+    public AtomicBoolean running = new AtomicBoolean();
+
+    public ClockHand (Tenori te){
+        this.te = te;
+    }
+    
+    public void run() {
+        System.out.println("Clockhand thread started");
+        running.set(true);
+
+        while(running.get()) {
+
+            for (int i=0; i<loopPoint; i++){
+                te. clockHandHighLight(i);
+
+                for(int j=0; j<SoundButton.getButtonsSelected().size(); j++)
+                {
+                    SoundButton button = SoundButton.getButtonsSelected().get(j);
+
+                    if (button.getXCoord() == i)
+                    {
+                    	
+
+                    }
+                }
+
+                if (!running.get())
+                {
+                	for (int j = 0; j < 16; j++){ //column
+           			 for (int k = 0; k < 16; k++ ) { //row
+                        te.matrix[j][k].Off();
+           			 }
+                    }
+                    break;
+                }
+
+                try {
+                    Thread.sleep(200);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
