5d19280fa0e8b323d82d030f227fe6478e13f73b
sup3rmuff1n <bradknoesen@gmail.com>
2016-03-07 08:39:29 +0000

    Update ChangeVoiceMode.java

--- ChangeVoiceMode.java
+++ ChangeVoiceMode.java
@@ -5,12 +5,14 @@ public class ChangeVoiceMode implements Mode{
     public ChangeVoiceMode(){
     }
     
-    public void changeVoice(soundButton,sound) {
-	int y = soundButton.y;
+    public void changeVoice(Layer layer, int sound) {
+	layer.setSound(sound);
+    }
 
+
+	/*
 	for (int x=0;x<16;x++){
 	    matrix[y][x].sound = sound;
 	}
-    }
-	
+	*/
 }
