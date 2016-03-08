3fb383b8b9a60caac0d8a7fce6229fb31b141eb2
sup3rmuff1n <bradknoesen@gmail.com>
2016-03-08 17:13:48 +0000

    Update ChangeVelocityMode.java

--- ChangeVelocityMode.java
+++ ChangeVelocityMode.java
@@ -1,4 +1,6 @@
-
+/*
+ * @authors Bradley Knoesen, Presley Kode
+ */
 
 public class ChangeVelocityMode implements Mode{
     
@@ -6,8 +8,13 @@ public class ChangeVelocityMode implements Mode{
     public ChangeVelocityMode(){}
     
     public chageVelocity(SoundButton soundButton,int veloc){
-      int y = soundButton.y;
-      for (int x=0;x<16;x++){
-        matrix[y][x].veloc = veloc;
-      }
+        int y = soundButton.y;
+        for (int x=0;x<16;x++){
+            matrix[y][x].veloc = veloc;
+        }
+        int x = soundButton.x;
+        for (int y=0;y<16;y++){
+            matrix[y][x].veloc = veloc;
+        }
+    }
 }
