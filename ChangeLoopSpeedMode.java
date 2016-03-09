42480f9d7ee1a0a1d112361fdd982a6578fb5db0
sup3rmuff1n <bradknoesen@gmail.com>
2016-03-09 11:57:08 +0000

    Update ChangeLoopSpeedMode.java

--- ChangeLoopSpeedMode.java
+++ ChangeLoopSpeedMode.java
@@ -1,6 +1,30 @@
 public class ChangeLoopSpeedMode implements Mode {
 
-  public ChangeLoopSpeedMode(){}
+  public ChangeLoopSpeedMode()
+  {
+    //Change Loop Speed Mode
+    // Methods to display correct message when the L3 button is clicked on.
+  	// Only displays message if button is enabled, else displays an error message. 
+    // @Kate Mawbey, Eneida Morina.       
+    Device de;
+    de.getTenori();
+    
+        de.L3.addMouseListener(new MouseAdapter() {
+        	public void mouseClicked(MouseEvent me){
+        		System.out.println("L3 button clicked");
+        		if (Device.getInstance().getMode() instanceof OffMode){
+        			JOptionPane.showMessageDialog(null, "Please turn on the Tenori!");
+        			return;
+        		}
+        		else {
+        			Device.getInstance().setMode(new ChangeLoopSpeedMode());
+        			de.L3.setSelected(true);
+        			clear();
+        		}
+        		
+        	}
+        });
+  }
   
   public void setLoopSpeed(Layer layer, int loopSpeed){
     layer.loopSpeed = loopSpeed;
