ffcfcc6ca44c347dda96cd7a7bb9261d9bbb9ef6
sup3rmuff1n <bradknoesen@gmail.com>
2016-03-09 11:30:24 +0000

    Update ChangeVoiceMode.java

--- ChangeVoiceMode.java
+++ ChangeVoiceMode.java
@@ -4,9 +4,35 @@
 
 public class ChangeVoiceMode implements Mode{
     
-    public ChangeVoiceMode(){}
+    public ChangeVoiceMode()
+    {
+    	private Device dev;
+    	dev.getTenori();
+    	
+    	dev.L1.addMouseListener(new MouseAdapter() {
+        	public void mouseClicked(MouseEvent me){
+        		System.out.println("L1 button clicked");
+        		if (Device.getInstance().getMode() instanceof OffMode){
+        			JOptionPane.showMessageDialog(null, "Please turn on the Tenori!");
+        			return;
+        		}
+        		else {
+				//Future implementation - moving from methods to classes. 
+        			Device.getInstance().setMode(new ChangeVoiceMode());
+        			//L1.setSelected(true);
+        			clear();
+        		}
+        		
+        	}
+        });
+    }
     
     public void changeVoice(Layer layer, int sound) {
 	layer.setSound(sound);
     }
+    
+    
+    
+    
+    
 }
