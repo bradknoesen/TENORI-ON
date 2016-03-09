e102e1a31a46a021343949baa372e0941244afad
sup3rmuff1n <bradknoesen@gmail.com>
2016-03-09 11:51:10 +0000

    Update ChangeVelocityMode.java

--- ChangeVelocityMode.java
+++ ChangeVelocityMode.java
@@ -5,7 +5,31 @@
 public class ChangeVelocityMode implements Mode{
     
     
-    public ChangeVelocityMode(){}
+    public ChangeVelocityMode()
+    {
+        //Change Velocity Mode
+        // Methods to display correct message when the L2 button is clicked on.
+	    // Only displays message if button is enabled, else displays an error message. 
+    	// @Kate Mawbey, Eneida Morina. 
+    	Device de;
+    	de.getTenori();
+    	
+        de.L2.addMouseListener(new MouseAdapter() {
+        	public void mouseClicked(MouseEvent me){
+        		System.out.println("L2 button clicked");
+        		if (Device.getInstance().getMode() instanceof OffMode){
+        			JOptionPane.showMessageDialog(null, "Please turn on the Tenori!");
+        			return;
+        		}
+        		else {
+        			Device.getInstance().setMode(new ChangeVelocityMode());
+        			L2.setSelected(true);
+        			clear();
+        		}
+        		
+        	}
+        });
+    }
     
     public chageVelocity(SoundButton soundButton,int veloc){
         int y = soundButton.y;
