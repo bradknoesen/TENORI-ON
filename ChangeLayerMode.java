725707f61e0cacb10e4fe980d54fd29af34ebea0
sup3rmuff1n <bradknoesen@gmail.com>
2016-03-09 11:59:09 +0000

    Update ChangeLayerMode.java

--- ChangeLayerMode.java
+++ ChangeLayerMode.java
@@ -1,7 +1,32 @@
 public class ChangeLayerMode implements Mode
 {
 
-  public ChangeLayerMode(){}
+  public ChangeLayerMode()
+  {
+    //Change Layer Mode
+        //
+	// Methods to display correct message when the R1 button is clicked on.
+	//Only displays message if button is enabled, else displays an error message. 
+	// @Kate Mawbey, Eneida Morina. 
+	  Device de;
+	  de.geTenori();
+	  
+        de.R1.addMouseListener(new MouseAdapter() {
+        	public void mouseClicked(MouseEvent me){
+        		System.out.println("R1 button clicked");
+        		if (Device.getInstance().getMode() instanceof OffMode){
+        			JOptionPane.showMessageDialog(null, "Please turn on the Tenori!");
+        			return;
+        		}
+        		else {
+        			Device.getInstance().setMode(new ChangeLayerMode());
+        			de.R1.setSelected(true);
+        			clear();
+        		}
+        		
+        	}
+        });
+  }
   
   public void changeLayer(Tenori tenori,int layerNum)
   {
