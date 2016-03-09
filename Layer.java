df49257bb27982ff6e7254b5f4d33020f68e7cab
sup3rmuff1n <bradknoesen@gmail.com>
2016-03-09 10:34:09 +0000

    Update Layer.java

--- Layer.java
+++ Layer.java
@@ -1,9 +1,16 @@
 public class Layer 
 {
 	private int sound;	//store sound in layer class
+	private int loopSpeed;
 	
-	public void setSound(int sound){
+	public void setSound(int sound)
+	{
 		this.sound = sound;
+	}
+	
+	public void setLoopSpeed(int loopSpeed)
+	{
+		this.loopSpeed = loopSpeed;
 	}
 	
 	private boolean[] buttonArray = new boolean[16 * 16];
